7136d00d87a447ba6faf77ea178402d13371ce20
Jocelyn Naarden <tzionone@gmail.com>
2022-03-28 03:10:11 -0400

    Assignment Completed 😶

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(arr, search) {
+  return (arr.filter((element) =>
+    element.toLowerCase() === search.toLowerCase()
+  ))
+}
+
+function fuzzyMatch(arr, search) {
+  // console.log(arr, search)
+  // return (arr.filter((element) => element[0] === search[0])) // does not take into account if second letter doesn't match
+  // console.log(arr.filter((element) => console.log(element.find(search))))
+  // if (element.find(search)) {
+  //   return element
+  // } // keeping these attempts
+  return arr.filter(el => el.indexOf(search) === 0)
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(el => {
+    return "ARR", el.name.toLowerCase() === string.toLowerCase()
+  })
+}
+
+
